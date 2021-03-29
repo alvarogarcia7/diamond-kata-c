cf3d533fa4751e059f139865430ef3514b283822
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-03-29 20:32:30 +0400

    Refactor: generalize the number of columns when generating output

--- prod.c
+++ prod.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void fill_pattern_A_(char **result_ptr, char c) {
-    *result_ptr = calloc(10, sizeof(char));
+#define ROW_TERMINATOR 1
+
+void fill_pattern_A_(char **result_ptr, char c, int columns) {
+    *result_ptr = calloc(columns, sizeof(char));
 
     char *result = *result_ptr;
 
@@ -13,8 +15,8 @@ void fill_pattern_A_(char **result_ptr, char c) {
     result[2] = ' ';
 }
 
-void fill_pattern_B_B(char **result_ptr, char c) {
-    *result_ptr = calloc(10, sizeof(char));
+void fill_pattern_B_B(char **result_ptr, char c, int columns) {
+    *result_ptr = calloc(columns, sizeof(char));
 
     char *result = *result_ptr;
 
@@ -31,21 +33,24 @@ int number_of_rows(char *chars) {
     return 1 + 2 * repeated;
 }
 
+int number_of_columns(char *chars){
+    return number_of_rows(chars);
+}
+
 char **diamond(char *chars){
     char **result;
 
-    result = calloc(number_of_rows(chars) + 1, sizeof(char *));
-    result[0] = calloc(10, sizeof(char));
+    result = calloc(number_of_rows(chars) + ROW_TERMINATOR, sizeof(char *));
     if(strlen(chars) == 1) {
-        result[0] = calloc(10, sizeof(char));
+        result[0] = calloc(number_of_columns(chars), sizeof(char));
         result[0][0] = chars[0];
         result[1] = NULL;
     } else if(strlen(chars) == 2){
-        fill_pattern_A_(&result[0], chars[0]);
+        fill_pattern_A_(&result[0], chars[0], number_of_columns(chars));
 
-        fill_pattern_B_B(&result[1], chars[1]);
+        fill_pattern_B_B(&result[1], chars[1], number_of_columns(chars));
 
-        fill_pattern_A_(&result[2], chars[0]);
+        fill_pattern_A_(&result[2], chars[0], number_of_columns(chars));
 
         result[3] = NULL;
     }
