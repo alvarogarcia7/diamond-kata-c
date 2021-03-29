9ff0942760d7cecdd6149ea8b21f490c998bd38f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-03-29 20:39:19 +0400

    Red: generate diamond for three letters

--- tests/test.c
+++ tests/test.c
@@ -28,6 +28,20 @@ static void two_letters(void **state)
     free(result);
 }
 
+static void three_letters(void **state)
+{
+    char **result = diamond("ABC");
+
+    assert_string_equal("  A  ", result[0]);
+    assert_string_equal(" B B ", result[1]);
+    assert_string_equal("C   C", result[2]);
+    assert_string_equal(" B B ", result[3]);
+    assert_string_equal("  A  ", result[4]);
+    assert_null(result[3]);
+
+    free(result);
+}
+
 
 /* A test case that does nothing and succeeds. */
 static void canary_test(void **state) {
@@ -39,6 +53,7 @@ int main(void) {
             cmocka_unit_test(canary_test)
             , cmocka_unit_test(single_letter)
             , cmocka_unit_test(two_letters)
+            , cmocka_unit_test(three_letters)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
