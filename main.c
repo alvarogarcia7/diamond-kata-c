1ca1b86048494b5531c26e942efdeaba84fd7508
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-03-30 09:58:26 +0400

    Create a main for printing the results

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main(){
-    printf("HELLO WORLD\n");
+#include <stdlib.h>
+#include "prod.h"
+
+void usage(char *program_name) {
+    printf("Usage: %s ALPHABET\n", program_name);
+    printf("Examp: %s ABCD\n", program_name);
+}
+
+int main(int argc, char** argv){
+    if(argc < 2){
+        usage(argv[0]);
+        return -1;
+    }
+
+    char *alphabet = argv[1];
+
+    char **result = diamond(alphabet);
+    int i = 0;
+    while(result[i] != NULL){
+        printf("%s\n", result[i]);
+        i++;
+    }
+
+    i = 0;
+    while(result[i] != NULL){
+        free(result[i]);
+        i++;
+    }
+    free(result);
     return 0;
 }
